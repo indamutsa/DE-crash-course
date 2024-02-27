ccd01f2cc52b6ee9a6b0e4c9b7000ed65e9d3c62
indamutsa <arsichizy@gmail.com>
2024-02-27 17:07:12 +0100

    data pipeline -- end

--- 1_basic-elt/start.sh
+++ 1_basic-elt/start.sh
@@ -1,3 +1,25 @@
-cron &
+docker compose up init airflow
 
-python3 /app/pipeline.py
+sleep 5
+
+docker compose up -d
+
+sleep
+
+cd airbyte
+
+
+if [ -d "../docker-compose.yaml" ]; then
+    docker compose up -d
+else
+    ./run-ab-platform.sh
+fi
+
+
+
+# ----
+
+# cron &
+
+# python3 /app/pipeline.py
+
